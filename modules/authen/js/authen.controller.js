d83c0fcc046b1a6e7020c0e7325330716d44b120
pobx <geidtiphong@gmail.com>
2018-02-26 16:10:14 +0700

    remove console.log

--- modules/authen/js/authen.controller.js
+++ modules/authen/js/authen.controller.js
@@ -12,7 +12,6 @@ angular.module('authen', ['localytics.directives', 'ngBootbox', 'toastersService
   function authenController($scope, authenFactory, $window) {
     // initialize param to object
     $scope.params = {};
-    console.log($window)
     $scope.authen = function () {
       let params = {
         username: $scope.params.username || 0,
@@ -30,7 +29,7 @@ angular.module('authen', ['localytics.directives', 'ngBootbox', 'toastersService
 
           let destination = "/miads-management/#/alumni";
           if (res.data.person_id == null) destination = "/miads-management-admin";
-          
+
           let landingUrl = "http://" + $window.location.host + destination;
           $window.location.href = landingUrl;
         });
