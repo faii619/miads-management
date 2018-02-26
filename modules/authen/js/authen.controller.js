46dbc3c5d0ba9d8a8cb6142a74d080a5e534a3ae
pobx <geidtiphong@gmail.com>
2018-02-26 16:02:21 +0700

    redirect after logeed OK

--- modules/authen/js/authen.controller.js
+++ modules/authen/js/authen.controller.js
@@ -7,19 +7,30 @@ angular.module('authen', ['localytics.directives', 'ngBootbox', 'toastersService
   angular.module('authen')
     .controller('authenController', authenController);
 
-  authenController.$inject = ['$scope', 'authenFactory'];
+  authenController.$inject = ['$scope', 'authenFactory', '$window'];
 
-  function authenController($scope, authenFactory) {
+  function authenController($scope, authenFactory, $window) {
     // initialize param to object
     $scope.params = {};
-
+    console.log($window)
     $scope.authen = function () {
       let params = {
         username: $scope.params.username || 0,
         passwords: $scope.params.passwords || 0,
       };
 
-      console.log(params)
+
+      authenFactory.authen(params)
+        .then(function (res) {
+
+          if (res.data.authen_status == 0) return;
+          let logged_profile = JSON.stringify(res.data);
+
+          localStorage.setItem('logged_profile', logged_profile);
+
+          let landingUrl = "http://" + $window.location.host + "/miads-management-admin";
+          $window.location.href = landingUrl;
+        });
     }
 
   }
