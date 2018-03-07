74e7d38590265ffbb4dfe472fb1a64eeda242988
pobx <geidtiphong@gmail.com>
2018-03-07 17:02:58 +0700

    remove some comment

--- modules/dashboard/js/dashboard.controller.js
+++ modules/dashboard/js/dashboard.controller.js
@@ -64,11 +64,9 @@ angular.module('dashboard', ['angularModalService', 'authen', 'mi_alumni_directo
       })
       .then(function (modal) {
         modal.element.modal();
+        // console.log('pop')
         modal.close.then(function (result) {
-          // if (result.status == 1) {
-          //   toasterService.toaster_success();
-          //   $scope.getDivision();
-          // }
+          console.log(result)
         });
       });
     }
