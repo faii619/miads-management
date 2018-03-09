312911b406166dbc65c25a4f82d49cd77e3e83a0
iviooThong <kitipot2538@gmail.com>
2018-03-09 16:07:10 +0700

    edit name image

--- modules/mi_alumni_directory/js/mi.alumni.directory.controller.js
+++ modules/mi_alumni_directory/js/mi.alumni.directory.controller.js
@@ -14,7 +14,6 @@ angular.module('mi_alumni_directory', ['angularModalService', 'ngBootbox', 'toas
     $scope.params = {};
     $scope.params.txt_id = (typeof $routeParams.param != 'undefined' ? $routeParams.param : 0);
     $scope.miAlumniDirectory = [];
-    $scope.content = [];
 
     // DataTables configurable options
     $scope.dtOptions = customsDataTables.dataTables();
@@ -23,28 +22,6 @@ angular.module('mi_alumni_directory', ['angularModalService', 'ngBootbox', 'toas
       miAlumniDirectoryFactory.getMiAlumniDirectory(null)
         .then(function (res) {
           $scope.miAlumniDirectory = res.data;
-          $scope.content = res.data[0]['content'];
-        });
-    }
-
-    $scope.submit = function () {
-      let params = {
-        id: 0,
-        content: $scope.content,
-      }
-
-      $ngBootbox.confirm('Do you want to save ?')
-        .then(function () {
-          miAlumniDirectoryFactory.setMiAlumniDirectoryContent(params)
-            .then(function (res) {
-              if (res.data.status == 1) {
-                toasterService.toaster_success();
-                $scope.miAlumniDirectory = res.data;
-                // $scope.getMiAlumniDirectory();
-              }
-            });
-        }, function () {
-          console.log('cancel');
         });
     }
   }
