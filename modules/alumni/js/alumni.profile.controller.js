21fca85f8e34e323c277514dde6025db6183a67c
faii619 <faiifylmus@gmail.com>
2018-03-21 11:19:39 +0700

    nothing

--- modules/alumni/js/alumni.profile.controller.js
+++ modules/alumni/js/alumni.profile.controller.js
@@ -21,6 +21,126 @@
     // DataTables configurable options
     $scope.dtOptions = customsDataTables.dataTables();
 
+    $scope.submit = function () {
+      let image = 0;
+      let image_size = 0;
+      // let old_image = 0;
+      let ministry = 0;
+      let universityName = 0;
+      let careerDepartment = 0;
+      let organizationName = 0;
+      let organizationDepartment = 0;
+
+      let national_id = (typeof $scope.params.national_id != 'undefined' && typeof $scope.params.national_id.id != 'undefined' ? $scope.params.national_id.id : 0);
+
+      let caption_home_id = (typeof $scope.params.caption_home_id != 'undefined' && typeof $scope.params.caption_home_id.id != 'undefined' ? $scope.params.caption_home_id.id : 0);
+
+      let caption_office_id = (typeof $scope.params.caption_office_id != 'undefined' && typeof $scope.params.caption_office_id.id != 'undefined' ? $scope.params.caption_office_id.id : 0);
+
+      let title_id = (typeof $scope.params.title_id != 'undefined' && typeof $scope.params.title_id.id != 'undefined' ? $scope.params.title_id.id : 0);
+
+      let organization_id = (typeof $scope.params.organization_id != 'undefined' && typeof $scope.params.organization_id.id != 'undefined' ? $scope.params.organization_id.id : 0);
+
+      if (organization_id == 1 || organization_id == 3) {
+        if (organization_id == 1) {
+          ministry = (typeof $scope.params.txt_career_ministry != 'undefined' ? $scope.params.txt_career_ministry : '0');
+          universityName = 0;
+        } else {
+          universityName = (typeof $scope.params.txt_career_university != 'undefined' ? $scope.params.txt_career_university : '0');
+          ministry = 0;
+        }
+        careerDepartment = (typeof $scope.params.txt_career_department != 'undefined' ? $scope.params.txt_career_department : '0');
+        organizationName = 0;
+        organizationDepartment = 0;
+      } else {
+        organizationName = (typeof $scope.params.txt_career_organization_name != 'undefined' ? $scope.params.txt_career_organization_name : '0');
+        organizationDepartment = (typeof $scope.params.txt_career_organization_department != 'undefined' ? $scope.params.txt_career_organization_department : '0')
+        ministry = 0;
+        universityName = 0;
+        careerDepartment = 0;
+      }
+
+      let params = {
+        id: $scope.data_user.person_id,
+        homeId: $scope.params.home_id,
+        officeId: $scope.params.office_id,
+        fileId: $scope.params.file_id,
+        imageSize: (typeof $scope.params.image != 'undefined' ? $scope.params.image.size : '0'),
+        image: (typeof $scope.params.image != 'undefined' ? $scope.params.image : '0'),
+        old_image: (typeof $scope.params.old_image != 'undefined' ? $scope.params.old_image : 0),
+        code: (typeof $scope.params.txt_code != 'undefined' ? $scope.params.txt_code : '0'),
+        homeStreetAddress: (typeof $scope.params.txt_home_street_address != 'undefined' ? $scope.params.txt_home_street_address : '0'),
+        homeCity: (typeof $scope.params.txt_home_city != 'undefined' ? $scope.params.txt_home_city : '0'),
+        homeProvince: (typeof $scope.params.txt_home_state_province != 'undefined' ? $scope.params.txt_home_state_province : '0'),
+        homeCountry: caption_home_id,
+        homeZip: (typeof $scope.params.txt_home_zip != 'undefined' ? $scope.params.txt_home_zip : '0'),
+        homeTel: (typeof $scope.params.txt_home_tel != 'undefined' ? $scope.params.txt_home_tel : '0'),
+        homeFax: (typeof $scope.params.txt_home_fax != 'undefined' ? $scope.params.txt_home_fax : '0'),
+        homeMobile: (typeof $scope.params.txt_home_mobile != 'undefined' ? $scope.params.txt_home_mobile : '0'),
+        officeStreetAddress: (typeof $scope.params.txt_office_street_address != 'undefined' ? $scope.params.txt_office_street_address : '0'),
+        officeCity: (typeof $scope.params.txt_office_city != 'undefined' ? $scope.params.txt_office_city : '0'),
+        officeProvince: (typeof $scope.params.txt_office_state_province != 'undefined' ? $scope.params.txt_office_state_province : '0'),
+        officeCountry: caption_office_id,
+        officeZip: (typeof $scope.params.txt_office_zip != 'undefined' ? $scope.params.txt_office_zip : '0'),
+        officeTel: (typeof $scope.params.txt_office_tel != 'undefined' ? $scope.params.txt_office_tel : '0'),
+        officeFax: (typeof $scope.params.txt_office_fax != 'undefined' ? $scope.params.txt_office_fax : '0'),
+        officeMobile: (typeof $scope.params.txt_office_mobile != 'undefined' ? $scope.params.txt_office_mobile : '0'),
+        title: title_id,
+        name: (typeof $scope.params.txt_name != 'undefined' ? $scope.params.txt_name : '0'),
+        gender: (typeof $scope.params.gender_id != 'undefined' ? $scope.params.gender_id : '0'),
+        birthday: (typeof $scope.params.txt_birth_date != 'undefined' ? $scope.params.txt_birth_date : '0'),
+        email: (typeof $scope.params.txt_active != 'undefined' ? $scope.params.txt_active : '0'),
+        otherEmails: (typeof $scope.params.txt_alternative != 'undefined' ? $scope.params.txt_alternative : '0'),
+        facebook: (typeof $scope.params.txt_facebook != 'undefined' ? $scope.params.txt_facebook : '0'),
+        twitter: (typeof $scope.params.txt_twitter != 'undefined' ? $scope.params.txt_twitter : '0'),
+        linked_in: (typeof $scope.params.txt_linked_in != 'undefined' ? $scope.params.txt_linked_in : '0'),
+        line: (typeof $scope.params.txt_line != 'undefined' ? $scope.params.txt_line : '0'),
+        ContactAddress: (typeof $scope.params.contact_id != 'undefined' ? $scope.params.contact_id : '0'),
+        nationCountry: national_id,
+        careerPosition: (typeof $scope.params.txt_career_position != 'undefined' ? $scope.params.txt_career_position : '0'),
+        careerStartYear: (typeof $scope.params.txt_career_start_date != 'undefined' ? $scope.params.txt_career_start_date : '0'),
+        careerExpertise: (typeof $scope.params.txt_career_area_of_expertise != 'undefined' ? $scope.params.txt_career_area_of_expertise : '0'),
+        careerMinistry: ministry,
+        careerDepartment: careerDepartment,
+        careerOrganizationName: organizationName,
+        careerOrganizationDepartment: organizationDepartment,
+        careerUniversityName: universityName,
+        careerUniversityDepartment: careerDepartment,
+        careerOrganizationType: organization_id,
+        careerDivision: (typeof $scope.params.txt_career_division != 'undefined' ? $scope.params.txt_career_division : '0'),
+      }
+
+      var options = {
+        message: 'Do you want to save ?',
+        // title: 'The title!',
+        className: 'test-class',
+        buttons: {
+          warning: {
+            label: "<i class=\"glyph-icon icon-times\"></i> Cancel",
+            className: "btn-warning",
+            callback: function () {
+              return;
+            }
+          },
+          success: {
+            label: "<i class=\"glyph-icon icon-save\"></i> save",
+            className: "btn-success",
+            callback: function () {
+              alumniFactory.setAlumni(params)
+                .then(function (res) {
+                  if (res.data.status == 1) {
+                    toasterService.toaster_success();
+                    $window.location.href = '#/alumni';
+                  }
+                });
+            }
+          }
+        }
+      };
+
+      $ngBootbox.customDialog(options);
+    }
+
     $scope.getAlumniProfile = function () {
       let id = (typeof $routeParams.param != 'undefined' ? $routeParams.param : $scope.data_user.person_id);
       alumniFactory.getAlumniProfile(id)
@@ -193,126 +313,6 @@
             $scope.params.organization_id = $scope.organization[index];
           }
         });
-    }
-
-    $scope.submit = function () {
-      let image = 0;
-      let image_size = 0;
-      // let old_image = 0;
-      let ministry = 0;
-      let universityName = 0;
-      let careerDepartment = 0;
-      let organizationName = 0;
-      let organizationDepartment = 0;
-
-      let national_id = (typeof $scope.params.national_id != 'undefined' && typeof $scope.params.national_id.id != 'undefined' ? $scope.params.national_id.id : 0);
-
-      let caption_home_id = (typeof $scope.params.caption_home_id != 'undefined' && typeof $scope.params.caption_home_id.id != 'undefined' ? $scope.params.caption_home_id.id : 0);
-
-      let caption_office_id = (typeof $scope.params.caption_office_id != 'undefined' && typeof $scope.params.caption_office_id.id != 'undefined' ? $scope.params.caption_office_id.id : 0);
-
-      let title_id = (typeof $scope.params.title_id != 'undefined' && typeof $scope.params.title_id.id != 'undefined' ? $scope.params.title_id.id : 0);
-
-      let organization_id = (typeof $scope.params.organization_id != 'undefined' && typeof $scope.params.organization_id.id != 'undefined' ? $scope.params.organization_id.id : 0);
-
-      if (organization_id == 1 || organization_id == 3) {
-        if (organization_id == 1) {
-          ministry = (typeof $scope.params.txt_career_ministry != 'undefined' ? $scope.params.txt_career_ministry : '0');
-          universityName = 0;
-        } else {
-          universityName = (typeof $scope.params.txt_career_university != 'undefined' ? $scope.params.txt_career_university : '0');
-          ministry = 0;
-        }
-        careerDepartment = (typeof $scope.params.txt_career_department != 'undefined' ? $scope.params.txt_career_department : '0');
-        organizationName = 0;
-        organizationDepartment = 0;
-      } else {
-        organizationName = (typeof $scope.params.txt_career_organization_name != 'undefined' ? $scope.params.txt_career_organization_name : '0');
-        organizationDepartment = (typeof $scope.params.txt_career_organization_department != 'undefined' ? $scope.params.txt_career_organization_department : '0')
-        ministry = 0;
-        universityName = 0;
-        careerDepartment = 0;
-      }
-
-      let params = {
-        id: $scope.data_user.person_id,
-        homeId: $scope.params.home_id,
-        officeId: $scope.params.office_id,
-        fileId: $scope.params.file_id,
-        imageSize: (typeof $scope.params.image != 'undefined' ? $scope.params.image.size : '0'),
-        image: (typeof $scope.params.image != 'undefined' ? $scope.params.image : '0'),
-        old_image: (typeof $scope.params.old_image != 'undefined' ? $scope.params.old_image : 0),
-        code: (typeof $scope.params.txt_code != 'undefined' ? $scope.params.txt_code : '0'),
-        homeStreetAddress: (typeof $scope.params.txt_home_street_address != 'undefined' ? $scope.params.txt_home_street_address : '0'),
-        homeCity: (typeof $scope.params.txt_home_city != 'undefined' ? $scope.params.txt_home_city : '0'),
-        homeProvince: (typeof $scope.params.txt_home_state_province != 'undefined' ? $scope.params.txt_home_state_province : '0'),
-        homeCountry: caption_home_id,
-        homeZip: (typeof $scope.params.txt_home_zip != 'undefined' ? $scope.params.txt_home_zip : '0'),
-        homeTel: (typeof $scope.params.txt_home_tel != 'undefined' ? $scope.params.txt_home_tel : '0'),
-        homeFax: (typeof $scope.params.txt_home_fax != 'undefined' ? $scope.params.txt_home_fax : '0'),
-        homeMobile: (typeof $scope.params.txt_home_mobile != 'undefined' ? $scope.params.txt_home_mobile : '0'),
-        officeStreetAddress: (typeof $scope.params.txt_office_street_address != 'undefined' ? $scope.params.txt_office_street_address : '0'),
-        officeCity: (typeof $scope.params.txt_office_city != 'undefined' ? $scope.params.txt_office_city : '0'),
-        officeProvince: (typeof $scope.params.txt_office_state_province != 'undefined' ? $scope.params.txt_office_state_province : '0'),
-        officeCountry: caption_office_id,
-        officeZip: (typeof $scope.params.txt_office_zip != 'undefined' ? $scope.params.txt_office_zip : '0'),
-        officeTel: (typeof $scope.params.txt_office_tel != 'undefined' ? $scope.params.txt_office_tel : '0'),
-        officeFax: (typeof $scope.params.txt_office_fax != 'undefined' ? $scope.params.txt_office_fax : '0'),
-        officeMobile: (typeof $scope.params.txt_office_mobile != 'undefined' ? $scope.params.txt_office_mobile : '0'),
-        title: title_id,
-        name: (typeof $scope.params.txt_name != 'undefined' ? $scope.params.txt_name : '0'),
-        gender: (typeof $scope.params.gender_id != 'undefined' ? $scope.params.gender_id : '0'),
-        birthday: (typeof $scope.params.txt_birth_date != 'undefined' ? $scope.params.txt_birth_date : '0'),
-        email: (typeof $scope.params.txt_active != 'undefined' ? $scope.params.txt_active : '0'),
-        otherEmails: (typeof $scope.params.txt_alternative != 'undefined' ? $scope.params.txt_alternative : '0'),
-        facebook: (typeof $scope.params.txt_facebook != 'undefined' ? $scope.params.txt_facebook : '0'),
-        twitter: (typeof $scope.params.txt_twitter != 'undefined' ? $scope.params.txt_twitter : '0'),
-        linked_in: (typeof $scope.params.txt_linked_in != 'undefined' ? $scope.params.txt_linked_in : '0'),
-        line: (typeof $scope.params.txt_line != 'undefined' ? $scope.params.txt_line : '0'),
-        ContactAddress: (typeof $scope.params.contact_id != 'undefined' ? $scope.params.contact_id : '0'),
-        nationCountry: national_id,
-        careerPosition: (typeof $scope.params.txt_career_position != 'undefined' ? $scope.params.txt_career_position : '0'),
-        careerStartYear: (typeof $scope.params.txt_career_start_date != 'undefined' ? $scope.params.txt_career_start_date : '0'),
-        careerExpertise: (typeof $scope.params.txt_career_area_of_expertise != 'undefined' ? $scope.params.txt_career_area_of_expertise : '0'),
-        careerMinistry: ministry,
-        careerDepartment: careerDepartment,
-        careerOrganizationName: organizationName,
-        careerOrganizationDepartment: organizationDepartment,
-        careerUniversityName: universityName,
-        careerUniversityDepartment: careerDepartment,
-        careerOrganizationType: organization_id,
-        careerDivision: (typeof $scope.params.txt_career_division != 'undefined' ? $scope.params.txt_career_division : '0'),
-      }
-
-      var options = {
-        message: 'Do you want to save ?',
-        // title: 'The title!',
-        className: 'test-class',
-        buttons: {
-          warning: {
-            label: "<i class=\"glyph-icon icon-times\"></i> Cancel",
-            className: "btn-warning",
-            callback: function () {
-              return;
-            }
-          },
-          success: {
-            label: "<i class=\"glyph-icon icon-save\"></i> save",
-            className: "btn-success",
-            callback: function () {
-              alumniFactory.setAlumni(params)
-                .then(function (res) {
-                  if (res.data.status == 1) {
-                    toasterService.toaster_success();
-                    $window.location.href = '#/alumni';
-                  }
-                });
-            }
-          }
-        }
-      };
-
-      $ngBootbox.customDialog(options);
     }
 
     $scope.getPrograms = function (programId = 0) {
